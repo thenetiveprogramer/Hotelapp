deac2c189685b1891f332d66030aa126a477c101
SWATI <swati1498@gmail.com>
2023-03-30 15:14:08 +0530

    commit1

---
+++ app/src/main/java/com/example/hotelapp2/activity_main13.kt
@@ -0,0 +1,11 @@
+package com.example.hotelapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_main13 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main13)
+    }
+}
---
+++ app/src/main/java/com/example/hotelapp2/activity_main4.kt
@@ -0,0 +1,11 @@
+package com.example.hotelapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_main4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+    }
+}
